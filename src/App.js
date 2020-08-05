bc50624a336d390798b0569ebaa95410d0ac38a1
Chrononox <tegbert@gmail.com>
2020-08-05 12:00:14 -0500

    added fetch for test users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import CardList from './CardList'
 
 class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      userList: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(user => {
+      this.setState({userList: user})
+    })
+  }
+
   render(){
+    console.log(this.state.userList)
     return(
       <div>
-        <h1>^.^</h1>
+        <h1>Friend-A-Dex</h1>
+        <CardList />
       </div>
     )
   }
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () =>{
+    return(
+        <p>Card Here</p>
+    )
+}
+
+export default Card
