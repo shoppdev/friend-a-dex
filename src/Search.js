540a2a6e408272584c660a2fed5044d928cbbf90
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-08-05 20:41:24 -0500

    fixed searchbox placeholder text

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Search = ({onSearch}) =>{
     return(
-        <input type="search" placeholder="Who you lookin' for?" onChange={onSearch}/>
+        <input type="search" placeholder="Who you lookn' for?" onChange={onSearch}/>
     )
 }
 
