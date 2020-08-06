9a90dd1185e5fcffb3302ee5cf5a156288e73241
Chrononox <tegbert@gmail.com>
2020-08-06 08:54:30 -0500

    Updates

---
+++ precache-manifest.dfde78e4b17dddcf52fdee06c0379a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf7e7e91e7ce76b41ab36538283cc25",
+    "url": "/friend-a-dex/index.html"
+  },
+  {
+    "revision": "747357439416e24e4589",
+    "url": "/friend-a-dex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d010a6f7ff754161d18b",
+    "url": "/friend-a-dex/static/css/main.c4aeaed6.chunk.css"
+  },
+  {
+    "revision": "747357439416e24e4589",
+    "url": "/friend-a-dex/static/js/2.6cdf8ba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friend-a-dex/static/js/2.6cdf8ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d010a6f7ff754161d18b",
+    "url": "/friend-a-dex/static/js/main.fb3ef9fd.chunk.js"
+  },
+  {
+    "revision": "174459bd6bf0560fe610",
+    "url": "/friend-a-dex/static/js/runtime-main.bb620d59.js"
+  }
+]);
